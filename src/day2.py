63cd3802dddee687cbe4ddda1afe8bdb4c5bda48
Gaute Kolstad <Gaute.Kolstad@dnvgl.com>
2020-12-02 08:47:05 +0100

    refactor day2

--- src/day2.py
+++ src/day2.py
@@ -1,6 +1,5 @@
 
 
-
 def main(source):
     sourceFile = open(source, 'r')
     lines = sourceFile.readlines()
@@ -14,21 +13,20 @@ def main(source):
 
 def checkPasword(lines):
     c = 0
-    for s in lines:
-        s = s.split(' ')
-        lowHigh = [int(x) for x in s[0].split('-')]
-        count = s[2].count(s[1][0])
+    for line in lines:
+        lowHigh, x, p = parseLine(line)
+
+        count = p.count(x)
         if count >= lowHigh[0] and count <= lowHigh[1]:
             c += 1
     return c
 
+
 def checkPasword2(lines):
     c = 0
-    for s in lines:
-        s = s.split(' ')
-        lowHigh = [int(x) for x in s[0].split('-')]
-        x = s[1][0]
-        p = s[2].rstrip('\n')
+    for line in lines:
+        lowHigh, x, p = parseLine(line)
+
         letters = [p[i-1] for i in lowHigh]
         if letters.count(x) == 1:
             c += 1
@@ -36,9 +34,16 @@ def checkPasword2(lines):
     return c
 
 
+def parseLine(line):
+    s = line.split(' ')
+    lowHigh = [int(x) for x in s[0].split('-')]
+    x = s[1][0]
+    p = s[2].rstrip('\n')
+
+    return lowHigh, x, p
+
 
 if __name__ == '__main__':
     challenge = '2-1'
     source = fr'..\resources\day{challenge}.txt'
     main(source)
-
