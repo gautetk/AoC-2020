fcf39a736ede080926879e6f8b10ff5bc040bcba
Gaute Kolstad <Gaute.Kolstad@dnvgl.com>
2020-12-05 20:17:22 +0100

    refactored day4

--- src/day4.py
+++ src/day4.py
@@ -5,16 +5,7 @@ def main(source):
     with open(source) as r:
         s = r.read()
 
-    infos = [
-        'byr',
-        'iyr',
-        'eyr',
-        'hgt',
-        'hcl',
-        'ecl',
-        'pid',
-        # 'cid',
-    ]
+    infos = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 
     result1 = part1(s, infos)
     print(result1)
@@ -32,72 +23,48 @@ def mDict(p):
     return {i.split(':')[0]: i.split(':')[1] for i in p}
 
 
-def minMaxInt(vmin, vmax, v):
+def minMaxInt(vMin, vMax, v):
     if not v.isdigit():
-        return 0
-    if int(v) < vmin or int(v) > vmax:
-        return 0
-    return 1
+        return False
+    return vMin <= int(v) <= vMax
+
+
+def validateHeight(hgt):
+    if hgt.endswith('cm'):
+        return minMaxInt(150, 193, hgt.strip('cm'))
+    elif hgt.endswith('in'):
+        return minMaxInt(59, 76, hgt.strip('in'))
+    else:
+        return False
 
 
 def validateValues(p):
-    if minMaxInt(1920, 2002, p['byr']) == 0:
-        return 0
-
-    if minMaxInt(2010, 2020, p['iyr']) == 0:
-        return 0
-
-    if minMaxInt(2020, 2030, p['eyr']) == 0:
-        return 0
-
-    hgt = p['hgt']
-    if hgt.strip('0123456789') == 'cm':
-        if minMaxInt(150, 193, hgt.strip('cm')) == 0:
-            return 0
-    elif hgt.strip('0123456789') == 'in':
-        if minMaxInt(59, 76, hgt.strip('in')) == 0:
-            return 0
-    else:
-        return 0
-
-    hcl = p['hcl']
-    if not hcl[0] == '#':
-        return 0
-    if not len(hcl) == 7:
-        return 0
-    if not re.match("^[a-z0-9_-]*$", hcl[1:]):
-        return 0
-
-    ecl = p['ecl']
-    if not ecl in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
-        return 0
-
-    pid = p['pid']
-    if not pid.isdigit():
-        return 0
-    if not len(pid) == 9:
-        return 0
-
-    return 1
+    valids = [
+        minMaxInt(1920, 2002, p['byr']),
+        minMaxInt(2010, 2020, p['iyr']),
+        minMaxInt(2020, 2030, p['eyr']),
+        validateHeight(p['hgt']),
+        re.match("^#[a-z0-9]{6}$", p['hcl']) != None,
+        p['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+        p['pid'].isdigit(),
+        len(p['pid']) == 9,
+    ]
+    return all(valids)
 
 
 def checkPassport1(infos, p):
-    if all(k in p for k in infos):
-        return 1
-    else:
-        return 0
+    return all(k in p for k in infos)
 
 
 def checkPassport2(infos, p):
     if all(k in p for k in infos):
         return validateValues(p)
     else:
-        return 0
+        return False
 
 
 def part1(s, infos):
     passports = parseInput(s)
-
     valid = [checkPassport1(infos, p) for p in passports]
 
     return sum(valid)
@@ -105,7 +72,6 @@ def part1(s, infos):
 
 def part2(s, infos):
     passports = parseInput(s)
-
     valid = [checkPassport2(infos, p) for p in passports]
 
     return sum(valid)
