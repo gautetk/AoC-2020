f75a3bd2c486de1d5d902fa079f515667721e458
Gaute Kolstad <Gaute.Kolstad@dnvgl.com>
2020-12-12 12:18:12 +0100

    day11 ugly version

--- src/day11.py
+++ src/day11.py
@@ -1,44 +1,191 @@
+import pprint
+
+
+from math import degrees
+
 
 def main(source):
     with open(source) as r:
         lines = r.readlines()
 
     inp = [x.rstrip('\n') for x in lines]
-    t = {'L': 0, '.': '.'}
-    inp = [[t['.']] * (len(inp[0])+2)] + [[t['.']] + [t[i] for i in x] + [t['.']] for x in inp] + [[t['.']] * (len(inp[0])+2)]
+    t = {'L': 0, '.': None}
+    seats = [[t['.']] * (len(inp[0])+2)] + [[t['.']] + [t[i] for i in x] + [t['.']] for x in inp] + [[t['.']] * (len(inp[0])+2)]
 
-    result1 = part1(inp)
+    result1 = part1(seats)
     print(result1)
 
-    # result2 = part2(inp)
-    # print(result2)
-
-# def checkSeat
+    result2 = part2(seats)
+    print(result2)
 
 
-def part1(inp):
-    
-    print(inp)
-    # inp.sort()
-    # diff = [j-i for i, j in zip(inp, inp[1:])]
-    # return diff.count(1) * diff.count(3)
+def checkSeat(square, seat):
+    if seat == 0 and sum(filter(None, square)) == 0:
+        return 1, 1
+
+    if seat == 1 and sum(filter(None, square)) >= 4:
+        return 1, 0
+    return 0, seat
 
 
-def part2(inp):
-    inp.sort()
-    diff = [j-i for i, j in zip(inp[:-1], inp[1:])]
+def interation(seats):
+    changes = 0
+    newSeats = []
+    for y in range(len(seats)):
+        if y == 0 or y == len(seats)-1:
+            newSeats.append([None]*len(seats[0]))
+        else:
+            row = []
+            for x in range(len(seats[0])):
+                if x == 0 or x == len(seats[0])-1:
+                    row.append(None)
+                else:
+                    if seats[y][x] == None:
+                        row.append(None)
+                    else:
+                        square = seats[y-1][x-1:x+2] + [seats[y][x-1], seats[y][x+1]] + seats[y+1][x-1:x+2]
+                        dChanges, seat = checkSeat(square, seats[y][x])
+                        changes += dChanges
+                        row.append(seat)
 
-    # partition list into sublist, split on 3
-    size = len(diff)
-    idx_list = [idx + 1 for idx, val in enumerate(diff) if val == 3]
-    zipIndex = zip([0] + idx_list, idx_list + ([size] if idx_list[-1] != size else []))
-    res = [len(diff[i: j-1]) for i, j in zipIndex]
+            newSeats.append(row)
+    return changes, newSeats
 
-    acc = 1
-    asfd = {0: 1, 1: 1, 2: 2, 3: 4, 4: 7}
-    for r in res:
-        acc = acc * asfd[r]
-    return acc
+
+def part1(seats):
+    changes = 1
+    while changes > 0:
+        changes, seats = interation(seats)
+    return sum([sum(filter(None, row)) for row in seats])
+
+
+def seeFn(x, y, seats):
+    c = 0
+    xTmp = x
+    yTmp = y
+    while xTmp > 0 and yTmp > 0:
+        xTmp += -1
+        yTmp += -1
+        if seats[yTmp][xTmp] == 1:
+            c += 1
+            break
+        if seats[yTmp][xTmp] == 0:
+            break
+
+    xTmp = x
+    yTmp = y
+    while xTmp > 0 and yTmp < len(seats)-1:
+        xTmp += -1
+        yTmp += 1
+        if seats[yTmp][xTmp] == 1:
+            c += 1
+            break
+        if seats[yTmp][xTmp] == 0:
+            break
+
+    xTmp = x
+    yTmp = y
+    while xTmp < len(seats[0])-1 and yTmp > 0:
+        xTmp += 1
+        yTmp += -1
+        if seats[yTmp][xTmp] == 1:
+            c += 1
+            break
+        if seats[yTmp][xTmp] == 0:
+            break
+
+    xTmp = x
+    yTmp = y
+    while xTmp < len(seats[0])-1 and yTmp < len(seats)-1:
+        xTmp += 1
+        yTmp += 1
+        if seats[yTmp][xTmp] == 1:
+            c += 1
+            break
+        if seats[yTmp][xTmp] == 0:
+            break
+
+    xTmp = x
+    yTmp = y
+    while xTmp < len(seats[0])-1:
+        xTmp += 1
+        if seats[yTmp][xTmp] == 1:
+            c += 1
+            break
+        if seats[yTmp][xTmp] == 0:
+            break
+
+    xTmp = x
+    yTmp = y
+    while xTmp > 0:
+        xTmp += -1
+        if seats[yTmp][xTmp] == 1:
+            c += 1
+            break
+        if seats[yTmp][xTmp] == 0:
+            break
+
+    xTmp = x
+    yTmp = y
+    while yTmp < len(seats)-1:
+        yTmp += 1
+        if seats[yTmp][xTmp] == 1:
+            c += 1
+            break
+        if seats[yTmp][xTmp] == 0:
+            break
+
+    xTmp = x
+    yTmp = y
+    while yTmp > 0:
+        yTmp += -1
+        if seats[yTmp][xTmp] == 1:
+            c += 1
+            break
+        if seats[yTmp][xTmp] == 0:
+            break
+
+    return c
+
+
+def checkSeat2(see, seat):
+    if seat == 0 and see == 0:
+        return 1, 1
+    if seat == 1 and see >= 5:
+        return 1, 0
+    return 0, seat
+
+
+def interation2(seats):
+    changes = 0
+    newSeats = []
+    for y in range(len(seats)):
+        if y == 0 or y == len(seats)-1:
+            newSeats.append([None]*len(seats[0]))
+        else:
+            row = []
+            for x in range(len(seats[0])):
+                if x == 0 or x == len(seats[0])-1:
+                    row.append(None)
+                else:
+                    if seats[y][x] == None:
+                        row.append(None)
+                    else:
+                        see = seeFn(x, y, seats)
+                        dChanges, seat = checkSeat2(see, seats[y][x])
+                        changes += dChanges
+                        row.append(seat)
+
+            newSeats.append(row)
+    return changes, newSeats
+
+
+def part2(seats):
+    changes = 1
+    while changes > 0:
+        changes, seats = interation2(seats)
+
+    return sum([sum(filter(None, row)) for row in seats])
 
 
 if __name__ == '__main__':
